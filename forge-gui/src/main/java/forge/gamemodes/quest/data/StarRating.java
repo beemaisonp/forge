357b009b27e6101b40dd2e33aab99d083d0746a1
beemaisonp <139219903+beemaisonp@users.noreply.github.com>
2023-08-14 09:48:09 -0700

    WIP comment out edition filtering in star ratings
    Only commented out, ideally add another checkbox which would turn use of filtering by set on/off.
    
    That or remove the use all together. Only con I see for removing filtering by edition is that certain releases of a card might have art that would be good/bad enough to shift a personal rating.

--- forge-gui/src/main/java/forge/gamemodes/quest/data/StarRating.java
+++ forge-gui/src/main/java/forge/gamemodes/quest/data/StarRating.java
@@ -9,7 +9,7 @@ public class StarRating {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((Edition == null) ? 0 : Edition.hashCode());
+        //result = prime * result + ((Edition == null) ? 0 : Edition.hashCode());
         result = prime * result + ((Name == null) ? 0 : Name.hashCode());
         result = prime * result + rating;
         return result;
@@ -28,6 +28,7 @@ public class StarRating {
         }
 
         StarRating other = (StarRating) obj;
+        /*
         if (Edition == null) {
             if (other.Edition != null) {
                 return false;
@@ -35,6 +36,7 @@ public class StarRating {
         } else if (!Edition.equals(other.Edition)) {
             return false;
         }
+         */
 
         if (Name == null) {
             if (other.Name != null) {
